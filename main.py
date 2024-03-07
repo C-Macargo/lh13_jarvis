dfe15a7bd47511f3fcfe12968d2ee577cccde8eb
Caio Camargo Silveira <caio.camargos97@gmail.com>
2024-03-06 22:21:16 -0300

    fix: token syntax + music cog help command

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import discord
 from discord.ext import commands
 import os, asyncio
 from music_cog import MusicCog  
+from help_cog import HelpCog
+from purge_cog import PurgeCog
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), case_insensitive=True)
+bot.remove_command('help')
 
 async def main():
+    print("LH13_jarvis is online")
     async with bot:
-        await bot.add_cog(MusicCog(bot))  
-        await bot.start(os.getenv('DISCORD_TOKEN'))  
+        await bot.add_cog(HelpCog(bot))
+        await bot.add_cog(MusicCog(bot))
+        await bot.add_cog(PurgeCog(bot))
+        await bot.start(os.getenv('DISCORD_TOKEN'))
+
 asyncio.run(main())
--- music_cog.py
+++ music_cog.py
@@ -8,7 +8,7 @@ class MusicCog(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(name='play')
+    @commands.command(name='play', help='Plays audio from a provided YOUTUBE URL in your voice channel.')
     async def play(self, ctx, *, url: str):
         if not ctx.author.voice:
             await ctx.send("You need to be in a voice channel to use this command.")
@@ -33,7 +33,7 @@ class MusicCog(commands.Cog):
         voice_client.play(audio_source, after=lambda e: print('Player error: %s' % e) if e else None)
         await ctx.send("Now playing audio.")
 
-    @commands.command(name='leave')
+    @commands.command(name='leave', help='Bot leaves the channel')
     async def leave(self, ctx):
         for vc in self.bot.voice_clients:
             if vc.guild == ctx.guild:

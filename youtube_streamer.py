17aa47d5e48baa4f6ca1ec73c3d72a114a067ba3
Caio Camargo Silveira <caio.camargos97@gmail.com>
2024-03-14 00:39:38 -0300

    fix: change format for longer music

--- youtube_streamer.py
+++ youtube_streamer.py
@@ -3,7 +3,7 @@ import yt_dlp
 class YouTubeStreamer:
     @staticmethod
     def get_audio_url(video_url):
-        with yt_dlp.YoutubeDL({'format': 'bestaudio', 'noplaylist': True}) as ydl:
+        with yt_dlp.YoutubeDL({'format': 'worstaudio', 'noplaylist': True}) as ydl:
             info_dict = ydl.extract_info(video_url, download=False)
             audio_url = info_dict.get('url', None)
             return audio_url
